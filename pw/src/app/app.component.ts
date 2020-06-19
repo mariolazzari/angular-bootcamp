659126585c42e0b7a27a0771aee591cc285a1dd3
Mario Lazzari <mario.lazzari@gmail.com>
2020-06-19 23:54:41 +0200

    Done lesson 16

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw';
+  length = 0;
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+  password = '';
+
+  onChangeLength(value: string) {
+    const parsed = parseInt(value);
+    if (!isNaN(parsed)) {
+      this.length = parsed;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+  onChangeUseSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onButtonClick() {
+    console.log(`Passowrd length: ${this.length}
+    - use letters: ${this.useLetters}
+    - use numbers: ${this.useNumbers}
+    - use symbols: ${this.useSymbols}`);
+
+    this.password = 'Button clicked';
+  }
 }
