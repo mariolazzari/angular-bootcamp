5ac55c57288a9eb4fc26a897f068dbbeead380a2
Mario Lazzari <mario.lazzari@gmail.com>
2020-06-22 19:34:13 +0200

    Done lesson 34

--- cards/src/app/card/card.component.ts
+++ cards/src/app/card/card.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  posts = [
+    {
+      title: 'Post 1',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today.',
+    },
+    {
+      title: 'Post 1',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today.',
+    },
+    {
+      title: 'Post 1',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today.',
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
