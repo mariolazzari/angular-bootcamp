9d575172217421ea883e3644f51199da79c4fdc8
Mario Lazzari <mario.lazzari@italcoeng.com>
2020-06-23 09:30:24 +0200

    Done lesson 50

---
+++ typing/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { lorem } from 'faker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  // on change event handler
+  onInput(value: string) {
+    this.enteredText = value;
+  }
+
+  // compare typings
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
+}
