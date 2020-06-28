f4d3dee88794087c5fdfa5814139b80f1e09a01b
Mario Lazzari <mario.lazzari@gmail.com>
2020-06-28 19:03:53 +0200

    Done lesson 80

--- pages/src/app/class.directive.ts
+++ pages/src/app/class.directive.ts
@@ -1,10 +1,12 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]',
 })
 export class ClassDirective {
-  constructor(private element: ElementRef) {
-    this.element.nativeElement.style.backgroundColor = 'orange';
+  constructor(private element: ElementRef) {}
+
+  @Input('appClass') set backgroundColor(color: string) {
+    this.element.nativeElement.style.backgroundColor = color;
   }
 }
