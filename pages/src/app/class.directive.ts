e7ef9b5c3cf677a04531184099861541b48e6db7
Mario Lazzari <mario.lazzari@gmail.com>
2020-06-28 21:11:22 +0200

    Done lesson 81

--- pages/src/app/class.directive.ts
+++ pages/src/app/class.directive.ts
@@ -6,7 +6,13 @@ import { Directive, ElementRef, Input } from '@angular/core';
 export class ClassDirective {
   constructor(private element: ElementRef) {}
 
-  @Input('appClass') set backgroundColor(color: string) {
-    this.element.nativeElement.style.backgroundColor = color;
+  @Input('appClass') set backgroundColor(classObj: any) {
+    for (let key in classObj) {
+      if (classObj[key]) {
+        this.element.nativeElement.classList.add(key);
+      } else {
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
   }
 }
