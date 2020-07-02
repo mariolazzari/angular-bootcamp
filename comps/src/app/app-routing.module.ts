f86edcac4a1ef6f808ac804a67346f14e8f7588c
Mario Lazzari <mario.lazzari@italcoeng.com>
2020-07-02 14:38:48 +0200

    Done lesson 102

--- comps/src/app/app-routing.module.ts
+++ comps/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- comps/src/app/app.module.ts
+++ comps/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 
 import { ElementsModule } from './elements/elements.module';
 import { CollectionsModule } from './collections/collections.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ElementsModule, CollectionsModule],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  imports: [BrowserModule, ElementsModule, CollectionsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
