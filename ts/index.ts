f0196ca42dca2a95c592fa2b77bfb26c5d5ec75a
Mario Lazzari <mario.lazzari@gmail.com>
2020-07-10 23:21:46 +0200

    Done lesson 150

---
+++ ts/index.ts
@@ -0,0 +1,11 @@
+// types
+const myName: string = "Mario";
+const anyType: any = undefined;
+const ten: number = 10;
+const yes: boolean = true;
+const nothing: null = null;
+const undef: undefined = undefined;
+
+// type inference
+const name2 = "Mario";
+// name = 10; -> error
