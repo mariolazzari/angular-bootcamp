fc7d603180761d7621e68fc1f9b7c47c5b71acc1
Mario Lazzari <mario.lazzari@gmail.com>
2020-07-11 11:22:13 +0200

    Done lesson 154

--- ts/index.ts
+++ ts/index.ts
@@ -9,3 +9,39 @@ const undef: undefined = undefined;
 // type inference
 const name2 = "Mario";
 // name = 10; -> error
+const sentence = "Mario is fine.";
+//sentence.includes("is");
+
+// functions
+const addNumbers = (a: number, b: number): number => a + b;
+const joinStrings = (a: string, b: string): string => a + b;
+
+// interfaces
+interface Post {
+  title: string;
+  daysOld: number;
+  published: boolean;
+}
+// object
+const post: Post = { title: "Post one", daysOld: 1, published: true };
+// method
+const printPost = (post: Post) => `${post.title} (${post.daysOld})`;
+
+// Class
+class Car {
+  color: string;
+  year: number;
+
+  constructor(color: string, year: number) {
+    this.color = color;
+    this.year = year;
+  }
+
+  drive() {
+    console.log("Vrooooom");
+  }
+}
+
+const car = new Car("red", 2020);
+car.drive();
+console.log(car);
