8f4206c5673a3b9d1385b2509e9ee99dba615698
Mario Lazzari <mario.lazzari@gmail.com>
2020-07-18 22:52:29 +0200

    Done lesson 211

--- photos/src/app/app.module.ts
+++ photos/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PhotoShowComponent } from './photo-show/photo-show.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoShowComponent
   ],
   imports: [
     BrowserModule
---
+++ photos/src/app/photo-show/photo-show.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-show',
+  templateUrl: './photo-show.component.html',
+  styleUrls: ['./photo-show.component.css']
+})
+export class PhotoShowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
