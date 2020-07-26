1b74ea3d6cbd7ebbae5a0b786c51fe13bc2b04fe
Mario Lazzari <mario.lazzari@gmail.com>
2020-07-26 20:11:47 +0200

    Done lesson 228

--- creditcard/src/app/app.module.ts
+++ creditcard/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
-  declarations: [AppComponent, CardFormComponent],
+  declarations: [AppComponent, CardFormComponent, InputComponent],
   imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ creditcard/src/app/input/input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css'],
+})
+export class InputComponent implements OnInit {
+  @Input() control: FormControl;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
