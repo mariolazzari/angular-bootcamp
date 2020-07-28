c460129785c7fe20cb75f2a55d10b82654988c3d
Mario Lazzari <mario.lazzari@gmail.com>
2020-07-28 20:34:29 +0200

    Done lesson 230

--- creditcard/src/app/input/input.component.ts
+++ creditcard/src/app/input/input.component.ts
@@ -8,8 +8,14 @@ import { FormControl } from '@angular/forms';
 })
 export class InputComponent implements OnInit {
   @Input() control: FormControl;
+  @Input() label: string;
 
   constructor() {}
 
   ngOnInit() {}
+
+  showErrors() {
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
+  }
 }
