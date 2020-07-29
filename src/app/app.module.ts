e16d6b8ff4f12f40ba241ce5d701ab793212a32e
Mario Lazzari <mario.lazzari@gmail.com>
2020-07-29 21:02:15 +0200

    Done lesson 233

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { CardFormComponent } from './card-form/card-form.component';
+import { InputComponent } from './input/input.component';
+
+@NgModule({
+  declarations: [AppComponent, CardFormComponent, InputComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
