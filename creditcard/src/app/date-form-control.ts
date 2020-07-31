4f10c29ad8f11aad933bdbf423cb84a6c19e1e76
Mario Lazzari <mario.lazzari@gmail.com>
2020-07-31 22:35:13 +0200

    Done lesson 238

--- creditcard/src/app/date-form-control.ts
+++ creditcard/src/app/date-form-control.ts
@@ -2,6 +2,21 @@ import { FormControl } from '@angular/forms';
 
 export class DateFormControl extends FormControl {
   setValue(value: string, options: any) {
+    if (value.match(/[^0-9|\/]/gi)) {
+      super.setValue(this.value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+
+    if (value.length > 5) {
+      super.setValue(this.value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+
+    if (value.length === 2 && this.value.length === 3) {
+      super.setValue(value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+
     if (value.length === 2) {
       super.setValue(value + '/', { ...options, emitModelToViewChange: true });
       return;
