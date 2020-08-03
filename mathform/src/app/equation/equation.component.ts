2bcd0b521e1b7f6293d18d03f761e54ee14a9c89
Mario Lazzari <mario.lazzari@gmail.com>
2020-08-03 20:15:56 +0200

    Done lesson 253

--- mathform/src/app/equation/equation.component.ts
+++ mathform/src/app/equation/equation.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-equation',
   templateUrl: './equation.component.html',
-  styleUrls: ['./equation.component.css']
+  styleUrls: ['./equation.component.css'],
 })
 export class EquationComponent implements OnInit {
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    answer: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  get a() {
+    return this.mathForm.value.a;
   }
 
+  get b() {
+    return this.mathForm.value.b;
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
+
+  ngOnInit(): void {}
 }
