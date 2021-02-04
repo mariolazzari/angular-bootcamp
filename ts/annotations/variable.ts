b5bf9abe3c65829fa365e381c2b292eef892484f
Mario Lazzari <mario.lazzari@gmail.com>
2021-02-04 19:01:03 +0100

    Done lesson 357

---
+++ ts/annotations/variable.ts
@@ -0,0 +1,20 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// obj
+let now: Date = new Date();
+
+// arrays
+let colors: string[] = ["red", "green", "blue"];
+let numbers: number[] = [1, 2, 3];
+let booleans: boolean[] = [true, false];
+
+// class
+class Car {}
+let car: Car = new Car();
+// obj literal
+let point: { x: number; y: number } = { x: 10, y: 20 };
