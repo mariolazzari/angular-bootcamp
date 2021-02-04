166ef13820d3a9ea1027292edfff2709c2f98a22
Mario Lazzari <mario.lazzari@gmail.com>
2021-02-04 20:04:41 +0100

    Done lesson 363

--- ts/annotations/variable.ts
+++ ts/annotations/variable.ts
@@ -18,3 +18,33 @@ class Car {}
 let car: Car = new Car();
 // obj literal
 let point: { x: number; y: number } = { x: 10, y: 20 };
+
+// fx
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// any type
+const json = { x: 19, y: 20 };
+const coordinates: { x: number; y: number } = JSON.parse(json.toString());
+console.log(coordinates);
+
+// declaration and init
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWord = true;
+  }
+}
+
+// no infer
+numbers = [-10, 1, -12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
