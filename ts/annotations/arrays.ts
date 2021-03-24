a11f76227493cbcc4310ed17147977271f00bccd
Mario Lazzari <mario.lazzari@gmail.com>
2021-03-24 22:32:45 +0100

    Done lesson 377

--- ts/annotations/arrays.ts
+++ ts/annotations/arrays.ts
@@ -5,3 +5,18 @@ const dates = [new Date(), new Date()];
 
 const carsByMakers = [["F40"], ["Yaris"], ["911"]];
 //const carsByMakers: string[][];
+
+const car1 = carMakers[0];
+const myCar = carMakers.pop();
+// error
+// carMakers.push(100)
+
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+// Flexible types
+const importantDates: (Date | string)[] = [];
+importantDates.push(new Date());
+importantDates.push("1975-03-28");
+//importantDates.push(100)
---
+++ ts/annotations/tuples.ts
@@ -0,0 +1,11 @@
+const drink = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40,
+};
+
+// type alias
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ["brown", true, 40];
+//pepsi[0] = 33
